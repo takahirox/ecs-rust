55c218887a91ad86e7b00d4423c6324953720791
Takahiro <hogehoge@gachapin.jp>
2021-09-19 01:07:25 -0700

    Add web collision example

---
+++ web/examples/collision/ecs_rust_web_example_collision.js
@@ -0,0 +1,392 @@
+
+let wasm;
+
+const heap = new Array(32).fill(undefined);
+
+heap.push(undefined, null, true, false);
+
+function getObject(idx) { return heap[idx]; }
+
+let heap_next = heap.length;
+
+function dropObject(idx) {
+    if (idx < 36) return;
+    heap[idx] = heap_next;
+    heap_next = idx;
+}
+
+function takeObject(idx) {
+    const ret = getObject(idx);
+    dropObject(idx);
+    return ret;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+
+function addHeapObject(obj) {
+    if (heap_next === heap.length) heap.push(heap.length + 1);
+    const idx = heap_next;
+    heap_next = heap[idx];
+
+    heap[idx] = obj;
+    return idx;
+}
+
+function debugString(val) {
+    // primitive types
+    const type = typeof val;
+    if (type == 'number' || type == 'boolean' || val == null) {
+        return  `${val}`;
+    }
+    if (type == 'string') {
+        return `"${val}"`;
+    }
+    if (type == 'symbol') {
+        const description = val.description;
+        if (description == null) {
+            return 'Symbol';
+        } else {
+            return `Symbol(${description})`;
+        }
+    }
+    if (type == 'function') {
+        const name = val.name;
+        if (typeof name == 'string' && name.length > 0) {
+            return `Function(${name})`;
+        } else {
+            return 'Function';
+        }
+    }
+    // objects
+    if (Array.isArray(val)) {
+        const length = val.length;
+        let debug = '[';
+        if (length > 0) {
+            debug += debugString(val[0]);
+        }
+        for(let i = 1; i < length; i++) {
+            debug += ', ' + debugString(val[i]);
+        }
+        debug += ']';
+        return debug;
+    }
+    // Test for built-in
+    const builtInMatches = /\[object ([^\]]+)\]/.exec(toString.call(val));
+    let className;
+    if (builtInMatches.length > 1) {
+        className = builtInMatches[1];
+    } else {
+        // Failed to match the standard '[object ClassName]'
+        return toString.call(val);
+    }
+    if (className == 'Object') {
+        // we're a user defined class or Object
+        // JSON.stringify avoids problems with cycles, and is generally much
+        // easier than looping through ownProperties of `val`.
+        try {
+            return 'Object(' + JSON.stringify(val) + ')';
+        } catch (_) {
+            return 'Object';
+        }
+    }
+    // errors
+    if (val instanceof Error) {
+        return `${val.name}: ${val.message}\n${val.stack}`;
+    }
+    // TODO we could test for more things here, like `Set`s and `Map`s.
+    return className;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+let cachedTextEncoder = new TextEncoder('utf-8');
+
+const encodeString = (typeof cachedTextEncoder.encodeInto === 'function'
+    ? function (arg, view) {
+    return cachedTextEncoder.encodeInto(arg, view);
+}
+    : function (arg, view) {
+    const buf = cachedTextEncoder.encode(arg);
+    view.set(buf);
+    return {
+        read: arg.length,
+        written: buf.length
+    };
+});
+
+function passStringToWasm0(arg, malloc, realloc) {
+
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length);
+        getUint8Memory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len);
+
+    const mem = getUint8Memory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3);
+        const view = getUint8Memory0().subarray(ptr + offset, ptr + len);
+        const ret = encodeString(arg, view);
+
+        offset += ret.written;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+
+function makeMutClosure(arg0, arg1, dtor, f) {
+    const state = { a: arg0, b: arg1, cnt: 1, dtor };
+    const real = (...args) => {
+        // First up with a closure we increment the internal reference
+        // count. This ensures that the Rust closure environment won't
+        // be deallocated while we're invoking it.
+        state.cnt++;
+        const a = state.a;
+        state.a = 0;
+        try {
+            return f(a, state.b, ...args);
+        } finally {
+            if (--state.cnt === 0) {
+                wasm.__wbindgen_export_2.get(state.dtor)(a, state.b);
+
+            } else {
+                state.a = a;
+            }
+        }
+    };
+    real.original = state;
+
+    return real;
+}
+function __wbg_adapter_16(arg0, arg1) {
+    wasm._dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf38eabc6feef6409(arg0, arg1);
+}
+
+/**
+*/
+export function start() {
+    wasm.start();
+}
+
+function isLikeNone(x) {
+    return x === undefined || x === null;
+}
+
+function handleError(f, args) {
+    try {
+        return f.apply(this, args);
+    } catch (e) {
+        wasm.__wbindgen_exn_store(addHeapObject(e));
+    }
+}
+
+function notDefined(what) { return () => { throw new Error(`${what} is not defined`); }; }
+
+async function load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+
+            } catch (e) {
+                if (module.headers.get('Content-Type') != 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else {
+                    throw e;
+                }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+
+        } else {
+            return instance;
+        }
+    }
+}
+
+async function init(input) {
+    if (typeof input === 'undefined') {
+        input = new URL('ecs_rust_web_example_collision_bg.wasm', import.meta.url);
+    }
+    const imports = {};
+    imports.wbg = {};
+    imports.wbg.__wbindgen_object_drop_ref = function(arg0) {
+        takeObject(arg0);
+    };
+    imports.wbg.__wbindgen_cb_drop = function(arg0) {
+        const obj = takeObject(arg0).original;
+        if (obj.cnt-- == 1) {
+            obj.a = 0;
+            return true;
+        }
+        var ret = false;
+        return ret;
+    };
+    imports.wbg.__wbindgen_string_new = function(arg0, arg1) {
+        var ret = getStringFromWasm0(arg0, arg1);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_instanceof_Window_c4b70662a0d2c5ec = function(arg0) {
+        var ret = getObject(arg0) instanceof Window;
+        return ret;
+    };
+    imports.wbg.__wbg_document_1c64944725c0d81d = function(arg0) {
+        var ret = getObject(arg0).document;
+        return isLikeNone(ret) ? 0 : addHeapObject(ret);
+    };
+    imports.wbg.__wbg_requestAnimationFrame_71638ca922068239 = function() { return handleError(function (arg0, arg1) {
+        var ret = getObject(arg0).requestAnimationFrame(getObject(arg1));
+        return ret;
+    }, arguments) };
+    imports.wbg.__wbg_getElementById_f3e94458ce77f0d0 = function(arg0, arg1, arg2) {
+        var ret = getObject(arg0).getElementById(getStringFromWasm0(arg1, arg2));
+        return isLikeNone(ret) ? 0 : addHeapObject(ret);
+    };
+    imports.wbg.__wbg_instanceof_CanvasRenderingContext2d_3abbe7ec7af32cae = function(arg0) {
+        var ret = getObject(arg0) instanceof CanvasRenderingContext2D;
+        return ret;
+    };
+    imports.wbg.__wbg_setfillStyle_528a6a267c863ae7 = function(arg0, arg1) {
+        getObject(arg0).fillStyle = getObject(arg1);
+    };
+    imports.wbg.__wbg_beginPath_733d5a9e3e769d24 = function(arg0) {
+        getObject(arg0).beginPath();
+    };
+    imports.wbg.__wbg_fill_dc4e97599365a189 = function(arg0) {
+        getObject(arg0).fill();
+    };
+    imports.wbg.__wbg_arc_bdfc39ad6001708b = function() { return handleError(function (arg0, arg1, arg2, arg3, arg4, arg5) {
+        getObject(arg0).arc(arg1, arg2, arg3, arg4, arg5);
+    }, arguments) };
+    imports.wbg.__wbg_clearRect_07caefec3496ced1 = function(arg0, arg1, arg2, arg3, arg4) {
+        getObject(arg0).clearRect(arg1, arg2, arg3, arg4);
+    };
+    imports.wbg.__wbg_instanceof_HtmlCanvasElement_25d964a0dde6717e = function(arg0) {
+        var ret = getObject(arg0) instanceof HTMLCanvasElement;
+        return ret;
+    };
+    imports.wbg.__wbg_width_555f63ab09ba7d3f = function(arg0) {
+        var ret = getObject(arg0).width;
+        return ret;
+    };
+    imports.wbg.__wbg_height_7153faec70fbaf7b = function(arg0) {
+        var ret = getObject(arg0).height;
+        return ret;
+    };
+    imports.wbg.__wbg_getContext_f701d0231ae22393 = function() { return handleError(function (arg0, arg1, arg2) {
+        var ret = getObject(arg0).getContext(getStringFromWasm0(arg1, arg2));
+        return isLikeNone(ret) ? 0 : addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbg_newnoargs_be86524d73f67598 = function(arg0, arg1) {
+        var ret = new Function(getStringFromWasm0(arg0, arg1));
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_call_888d259a5fefc347 = function() { return handleError(function (arg0, arg1) {
+        var ret = getObject(arg0).call(getObject(arg1));
+        return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbindgen_object_clone_ref = function(arg0) {
+        var ret = getObject(arg0);
+        return addHeapObject(ret);
+    };
+    imports.wbg.__wbg_self_c6fbdfc2918d5e58 = function() { return handleError(function () {
+        var ret = self.self;
+        return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbg_window_baec038b5ab35c54 = function() { return handleError(function () {
+        var ret = window.window;
+        return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbg_globalThis_3f735a5746d41fbd = function() { return handleError(function () {
+        var ret = globalThis.globalThis;
+        return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbg_global_1bc0b39582740e95 = function() { return handleError(function () {
+        var ret = global.global;
+        return addHeapObject(ret);
+    }, arguments) };
+    imports.wbg.__wbindgen_is_undefined = function(arg0) {
+        var ret = getObject(arg0) === undefined;
+        return ret;
+    };
+    imports.wbg.__wbg_random_a582babfa4489c72 = typeof Math.random == 'function' ? Math.random : notDefined('Math.random');
+    imports.wbg.__wbindgen_debug_string = function(arg0, arg1) {
+        var ret = debugString(getObject(arg1));
+        var ptr0 = passStringToWasm0(ret, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var len0 = WASM_VECTOR_LEN;
+        getInt32Memory0()[arg0 / 4 + 1] = len0;
+        getInt32Memory0()[arg0 / 4 + 0] = ptr0;
+    };
+    imports.wbg.__wbindgen_throw = function(arg0, arg1) {
+        throw new Error(getStringFromWasm0(arg0, arg1));
+    };
+    imports.wbg.__wbindgen_closure_wrapper106 = function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 31, __wbg_adapter_16);
+        return addHeapObject(ret);
+    };
+
+    if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {
+        input = fetch(input);
+    }
+
+
+
+    const { instance, module } = await load(await input, imports);
+
+    wasm = instance.exports;
+    init.__wbindgen_wasm_module = module;
+    wasm.__wbindgen_start();
+    return wasm;
+}
+
+export default init;
+
---
+++ web/examples/collision/src/lib.rs
@@ -0,0 +1,253 @@
+use std::f64;
+use std::cell::RefCell;
+use std::rc::Rc;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+use ecs_rust::world::World;
+use ecs_rust::component::Component;
+use ecs_rust::component_manager::ComponentsManager;
+use ecs_rust::system::System;
+
+const CANVAS_ID: &str = "canvas";
+
+#[wasm_bindgen]
+extern "C" {
+	#[wasm_bindgen(js_namespace = console)]
+	fn log(s: &str);
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+	let window = web_sys::window().expect("no global `window` exists");
+	window
+		.request_animation_frame(f.as_ref().unchecked_ref())
+		.expect("should register `requestAnimationFrame` OK");
+}
+
+fn rand() -> f64 {
+	js_sys::Math::random()
+}
+
+fn get_canvas() -> web_sys::HtmlCanvasElement {
+	let document = web_sys::window().unwrap().document().unwrap();
+	document.get_element_by_id(CANVAS_ID)
+		.unwrap()
+		.dyn_into::<web_sys::HtmlCanvasElement>()
+		.map_err(|_| ())
+		.unwrap()
+}
+
+fn get_context() -> web_sys::CanvasRenderingContext2d {
+	get_canvas()
+		.get_context("2d")
+		.unwrap()
+		.unwrap()
+		.dyn_into::<web_sys::CanvasRenderingContext2d>()
+		.unwrap()
+}
+
+struct CanvasSize {
+	width: f64,
+	height: f64
+}
+
+struct Position {
+	x: f64,
+	y: f64
+}
+
+struct Velocity {
+	x: f64,
+	y: f64
+}
+
+struct Circle {
+	radius: f64
+}
+
+struct Collidable {
+	collided: bool
+}
+
+impl Component for CanvasSize {
+}
+
+impl Component for Position {
+}
+
+impl Component for Velocity {
+}
+
+impl Component for Circle {
+}
+
+impl Component for Collidable {
+}
+
+struct MoveSystem {
+}
+
+struct ReflectBoundarySystem {
+}
+
+struct CollisionCheckSystem {
+}
+
+struct RenderSystem {
+}
+
+impl System for MoveSystem {
+	fn update(&mut self, manager: &mut ComponentsManager) {
+		let ids = manager.get_entity_ids_for_pair::<Position, Velocity>();
+		for id in ids.iter() {
+			let (position, velocity) = manager.borrow_component_pair_mut::<Position, Velocity>(*id).unwrap();
+			position.x += velocity.x;
+			position.y += velocity.y;
+		}
+	}
+}
+
+impl System for ReflectBoundarySystem {
+	fn update(&mut self, manager: &mut ComponentsManager) {
+		let (canvas_width, canvas_height) = {
+			let canvas_size = &manager.borrow_components::<CanvasSize>().unwrap()[0];
+			(canvas_size.width, canvas_size.height)
+		};
+		let ids = manager.get_entity_ids_for_triple::<Position, Velocity, Circle>();
+		for id in ids.iter() {
+			let (position, velocity, circle) = manager.borrow_component_triple_mut::<Position, Velocity, Circle>(*id).unwrap();
+			if position.x - circle.radius < 0.0 ||
+				position.x + circle.radius >= canvas_width {
+				velocity.x = -velocity.x;
+			}
+			if position.y - circle.radius < 0.0 ||
+				position.y + circle.radius >= canvas_height {
+				velocity.y = -velocity.y;
+			}
+		}
+	}
+}
+
+impl System for CollisionCheckSystem {
+	fn update(&mut self, manager: &mut ComponentsManager) {
+		let ids = manager.get_entity_ids_for_triple::<Position, Circle, Collidable>();
+		for id in ids.iter() {
+			let collidable = manager.borrow_component_mut::<Collidable>(*id).unwrap();
+			collidable.collided = false;
+		}
+		for i in 0..ids.len() {
+			for j in (i+1)..ids.len() {
+				if self.check_collision(manager, ids[i], ids[j]) {
+					let collidable = manager.borrow_component_mut::<Collidable>(ids[i]).unwrap();
+					collidable.collided = true;
+					let collidable = manager.borrow_component_mut::<Collidable>(ids[j]).unwrap();
+					collidable.collided = true;
+				}
+			}
+		}
+	}
+}
+
+impl CollisionCheckSystem {
+	fn check_collision(&self, manager: &ComponentsManager, entity_id1: usize, entity_id2: usize) -> bool {
+		let position1 = manager.borrow_component::<Position>(entity_id1).unwrap();
+		let circle1 = manager.borrow_component::<Circle>(entity_id1).unwrap();
+		let position2 = manager.borrow_component::<Position>(entity_id2).unwrap();
+		let circle2 = manager.borrow_component::<Circle>(entity_id2).unwrap();
+		let dx = position1.x - position2.x;
+		let dy = position1.y - position2.y;
+		(dx * dx + dy * dy).sqrt() < (circle1.radius + circle2.radius)
+	}
+}
+
+impl System for RenderSystem {
+	fn update(&mut self, manager: &mut ComponentsManager) {
+		let (canvas_width, canvas_height) = {
+			let canvas_size = &manager.borrow_components::<CanvasSize>().unwrap()[0];
+			(canvas_size.width, canvas_size.height)
+		};
+
+		// @TODO: Is getting context every frame costly?
+		let context = get_context();
+		context.clear_rect(0.0, 0.0, canvas_width, canvas_height);
+
+		let ids = manager.get_entity_ids_for_triple::<Position, Circle, Collidable>();
+		for id in ids.iter() {
+			let position = manager.borrow_component::<Position>(*id).unwrap();
+			let circle = manager.borrow_component::<Circle>(*id).unwrap();
+			let collidable = manager.borrow_component::<Collidable>(*id).unwrap();
+
+			let color_str = match collidable.collided {
+				true => "red",
+				false => "black"
+			};
+
+			context.begin_path();
+			context.set_fill_style(&JsValue::from_str(color_str));
+			context
+				.arc(position.x, position.y, circle.radius, 0.0, f64::consts::PI * 2.0)
+				.unwrap();
+			context.fill();
+		}
+	}
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+	let (canvas_width, canvas_height) = {
+		let canvas = get_canvas();
+		(canvas.width() as f64, canvas.height() as f64)
+	};
+
+	let mut world = World::new();
+
+	world
+		.register_component::<CanvasSize>()
+		.register_component::<Position>()
+		.register_component::<Velocity>()
+		.register_component::<Circle>()
+		.register_component::<Collidable>();
+
+	{
+		let entity_id = world.create_entity();
+		world
+			.add_component_to_entity(entity_id, CanvasSize {
+				width: canvas_width,
+				height: canvas_height
+			});
+	}
+
+	for _i in 0..50 {
+		let entity_id = world.create_entity();
+		world
+			.add_component_to_entity(entity_id, Position {
+				x: 25.0 + rand() * (canvas_width - 45.0),
+				y: 25.0 + rand() * (canvas_height - 45.0)
+			})
+			.add_component_to_entity(entity_id, Velocity {
+				x: rand() * 5.0 - 2.5,
+				y: rand() * 5.0 - 2.5
+			})
+			.add_component_to_entity(entity_id, Circle {
+				radius: 5.0 + (rand() * 15.0)
+			})
+			.add_component_to_entity(entity_id, Collidable {
+				collided: false
+			});
+	}
+
+	world.add_system(MoveSystem {});
+	world.add_system(ReflectBoundarySystem {});
+	world.add_system(CollisionCheckSystem {});
+	world.add_system(RenderSystem {});
+
+	let f = Rc::new(RefCell::new(None));
+	let g = f.clone();
+
+	*g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+		request_animation_frame(f.borrow().as_ref().unwrap());
+		world.update();
+	}) as Box<dyn FnMut()>));
+
+	request_animation_frame(g.borrow().as_ref().unwrap());
+}
